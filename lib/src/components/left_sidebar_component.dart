44403cb6ecc53ab7dedcd508862e1967daf1dabe
nazrul78 <nnakib78@gmail.com>
2024-06-13 15:40:46 +0600

    Modified left sidebar component

--- lib/src/components/left_sidebar_component.dart
+++ lib/src/components/left_sidebar_component.dart
@@ -1,25 +1,191 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:network_topology_gis/src/config/app_theme.dart';
+import 'package:network_topology_gis/src/helpers/hex_color.dart';
+import 'package:network_topology_gis/src/helpers/k_text.dart';
 
 class LeftSidebarComponent extends StatelessWidget {
   const LeftSidebarComponent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
+    return Drawer(
       backgroundColor: Colors.white,
       child: Column(
         children: [
           Expanded(
+            flex: 1,
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  Text('Hello>>>>>>>'),
-                  Text('Hello2>>>>>>'),
+                  Container(
+                    //color: hexToColor('#95C08B'),
+                    color: AppTheme.appThemeColor,
+                    width: Get.width,
+                    height: 250,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            //color: Colors.white,
+                            borderRadius: BorderRadius.circular(45),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black26,
+                                spreadRadius: 5,
+                                blurRadius: 9,
+                                offset: Offset(0, 0),
+                              ),
+                            ],
+                          ),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 45,
+                            child: CircleAvatar(
+                              backgroundColor: hexToColor('#95C08B'),
+                              radius: 43,
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(50),
+                                  child: FlutterLogo(
+                                    size: 80,
+                                  )
+
+                                  // userC.userProfileImage.value != null
+                                  //     ? Image.memory(
+                                  //         userC.userProfileImage.value!,
+                                  //         fit: BoxFit.cover,
+                                  //         cacheWidth: 100,
+                                  //         cacheHeight: 100,
+                                  //       )
+                                  //     : RenderSvg(path: 'avatar_placeholder'),
+                                  ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 22,
+                        ),
+                        KText(
+                          text: 'Nazrul Islam',
+                          // text: Base.userController.currentUser.value != null
+                          //     ? Base.userController.currentUser.value!.fullName
+                          //     : '',
+                          bold: true,
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                        KText(
+                          text: 'Araihazar, Narayenganj',
+                          // text: Base.userController.currentUser.value != null
+                          //     ? Base.userController.currentUser.value!.address
+                          //     : '',
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ],
+                    ),
+                  ),
+                  CustomListTile(
+                    Icon(Icons.person, size: 25, color: Colors.black54),
+                    'Update My Profile',
+                    () {
+                      Get.snackbar(
+                          'Attention!!', 'This feature is not available yet.',
+                          snackPosition: SnackPosition.BOTTOM,
+                          backgroundColor: Colors.white,
+                          colorText: Colors.redAccent);
+                    },
+                  ),
+                  CustomListTile(
+                    Icon(Icons.change_circle_outlined,
+                        size: 25, color: Colors.black54),
+                    'Change My Password',
+                    () {
+                      Get.snackbar(
+                          'Attention!!', 'This feature is not available yet.',
+                          snackPosition: SnackPosition.BOTTOM,
+                          backgroundColor: Colors.white,
+                          colorText: Colors.redAccent);
+                    },
+                  ),
+                  CustomListTile(
+                    Icon(Icons.logout_outlined,
+                        size: 25, color: Colors.black54),
+                    'Logout',
+                    () async {
+                      // offAll(LoginPage());
+                      // Base.authController.logout();
+                      // Get.offAll(() => LoginPage());
+                    },
+                  ),
                 ],
               ),
             ),
           ),
+          SizedBox(height: 5),
+          Expanded(
+            flex: 0,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: KText(
+                text: 'Version 1.0.0',
+                bold: true,
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
         ],
+      ),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  final Widget menuIcon;
+  final String text;
+  final void Function() onTap;
+
+  const CustomListTile(this.menuIcon, this.text, this.onTap);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey.shade400))),
+        child: InkWell(
+          splashColor: hexToColor('#95C08B').withOpacity(.7),
+          borderRadius: BorderRadius.circular(5),
+          onTap: onTap,
+          child: SizedBox(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    // Icon(icon),
+                    menuIcon,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                    ),
+                    KText(
+                      text: text,
+                      fontSize: 16,
+                      bold: true,
+                    )
+                  ],
+                ),
+                //Icon(Icons.arrow_right)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/src/data/area_dropdown_data.dart
@@ -0,0 +1,116 @@
+List<Map<String, dynamic>> areaDropdowns = [
+  {
+    'name': 'Bagerhat',
+    'code': 'BD4001',
+    'areas': [
+      {'name': 'Barai Para', 'code': 'BD40010817'},
+      {'name': 'Bemarta', 'code': 'BD40010825'},
+      {'name': 'Bishnupur', 'code': 'BD40010834'},
+      {'name': 'Chandpai Range', 'code': 'BD40015897'},
+      {'name': 'Dema', 'code': 'BD40010835'},
+      {'name': 'Dhopakhali', 'code': 'BD40013828'},
+      {'name': 'Fakirhat', 'code': 'BD40013431'},
+      {'name': 'Gota Para', 'code': 'BD40010851'},
+      {'name': 'Jatrapur', 'code': 'BD40010860'},
+      {'name': 'Kara Para', 'code': 'BD40010869'},
+      {'name': 'Khanpur', 'code': 'BD40010877'},
+      {'name': 'Malliker Ber', 'code': 'BD40017353'},
+      {'name': 'Mulghar', 'code': 'BD40013452'},
+      {'name': 'Piljanga', 'code': 'BD40013473'},
+      {'name': 'Rakhalgachhi', 'code': 'BD40010886'},
+      {'name': 'Rari Para', 'code': 'BD40013876'},
+      {'name': 'Sharankhola Range', 'code': 'BD40017797'},
+      {'name': 'Shat Gambuj', 'code': 'BD40010894'},
+      {'name': 'Udaypur', 'code': 'BD40015695'},
+      {'name': 'Ujalkur', 'code': 'BD40017394'}
+    ]
+  },
+  {
+    'name': 'Bandarban',
+    'code': 'BD2003',
+    'areas': [
+      {'name': 'Bandarban', 'code': 'BD20031415'},
+      {'name': 'Kuhalong', 'code': 'BD20031431'},
+      {'name': 'Naikhongchhari', 'code': 'BD20037376'},
+      {'name': 'Rajbila', 'code': 'BD20031447'},
+      {'name': 'Swalak', 'code': 'BD20031463'},
+      {'name': 'Tankabati', 'code': 'BD20031479'}
+    ]
+  },
+  {
+    'name': 'Barguna',
+    'code': 'BD1004',
+    'areas': [
+      {'name': 'Ayla Patakata', 'code': 'BD10042817'},
+      {'name': 'Badarkhali', 'code': 'BD10042819'},
+      {'name': 'Barguna', 'code': 'BD10042828'},
+      {'name': 'Burir Char', 'code': 'BD10042838'},
+      {'name': 'Dhalua', 'code': 'BD10042847'},
+      {'name': 'Gaurichanna', 'code': 'BD10042866'},
+      {'name': 'Keorabunia', 'code': 'BD10042876'},
+      {'name': 'M.baliatali', 'code': 'BD10042885'},
+      {'name': 'Naltona', 'code': 'BD10042895'},
+      {'name': 'Phuljhury', 'code': 'BD10042857'}
+    ]
+  },
+  {
+    'name': 'Barisal',
+    'code': 'BD1006',
+    'areas': [
+      {'name': 'Alimabad', 'code': 'BD10066213'},
+      {'name': 'Andhar Manik', 'code': 'BD10066215'},
+      {'name': 'Chandpura', 'code': 'BD10065132'},
+      {'name': 'Char Baria', 'code': 'BD10065134'},
+      {'name': 'Char Kowa', 'code': 'BD10065143'},
+      {'name': 'Gaila', 'code': 'BD10060247'},
+      {'name': 'Hizla Gaurabdi', 'code': 'BD10063681'},
+      {'name': 'Jagua', 'code': 'BD10065160'},
+      {'name': 'Kashipur', 'code': 'BD10065169'},
+      {'name': 'Roy Pasha Karapur', 'code': 'BD10065177'},
+      {'name': 'Shayestabad', 'code': 'BD10065186'},
+      {'name': 'Tungibaria', 'code': 'BD10065194'}
+    ]
+  },
+  {
+    'name': 'Bhola',
+    'code': 'BD1009',
+    'areas': [
+      {'name': 'Alinagar', 'code': 'BD10091812'},
+      {'name': 'Aminabad', 'code': 'BD10092515'},
+      {'name': 'Bapta', 'code': 'BD10091814'},
+      {'name': 'Bhelu Miah', 'code': 'BD10091894'},
+      {'name': 'Char Kalmi', 'code': 'BD10092528'},
+      {'name': 'Char Pata', 'code': 'BD10092928'},
+      {'name': 'Char Samaia', 'code': 'BD10091821'},
+      {'name': 'Char Shibpur', 'code': 'BD10091829'},
+      {'name': 'Dakshin Dighaldi', 'code': 'BD10091880'},
+      {'name': 'Dhal Char', 'code': 'BD10092551'},
+      {'name': 'Dhania', 'code': 'BD10091836'},
+      {'name': 'Kachia', 'code': 'BD10091858'},
+      {'name': 'Paschim Ilisha', 'code': 'BD10091855'},
+      {'name': 'Rajapur', 'code': 'BD10091873'},
+      {'name': 'Uttar Dighaldi', 'code': 'BD10091865'},
+      {'name': 'Veduria', 'code': 'BD10091887'}
+    ]
+  },
+  {
+    'name': 'Bogra',
+    'code': 'BD5010',
+    'areas': [
+      {'name': 'Bhelabari', 'code': 'BD50108111'},
+      {'name': 'Erulia', 'code': 'BD50102023'},
+      {'name': 'Fapore', 'code': 'BD50102025'},
+      {'name': 'Fulbari', 'code': 'BD50108131'},
+      {'name': 'Gokul', 'code': 'BD50102034'},
+      {'name': 'Hat Sherpur', 'code': 'BD50108137'},
+      {'name': 'Kamalpur', 'code': 'BD50108144'},
+      {'name': 'Karnibari', 'code': 'BD50108156'},
+      {'name': 'Kutubpur', 'code': 'BD50108163'},
+      {'name': 'Lahiri Para', 'code': 'BD50102047'},
+      {'name': 'Namuja', 'code': 'BD50102060'},
+      {'name': 'Narchi', 'code': 'BD50108175'},
+      {'name': 'Nishindara', 'code': 'BD50102064'},
+      {'name': 'Noongo', 'code': 'BD50102066'}
+    ]
+  }
+];
